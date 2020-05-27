bf96c678163e32c8005466f12a91814e2b7c30fc
airwolff <andy.wolff@gmail.com>
2020-05-27 15:12:41 -0500

    js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,12 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
+function add(firstNumber, secondNumber) {
+    const result = firstNumber + secondNumber;
+    return result;
+}
+
+currentResult = add(1, 1);
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
