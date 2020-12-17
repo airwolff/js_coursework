8b07e26bb8084479ddda21dec7913fe6669b2d54
airwolff <andy.wolff@gmail.com>
2020-12-17 12:36:23 -0600

    ?

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,41 +1,47 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
+// gets user input
 function getUserNumberInput() {
     return parseInt(userInput.value);
 }
 
+// generatess and writes calculation log
 function createAndWriteLog(operator, resultBeforeCalc, calcNumber) {
     const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-    outputResult(currentResult, calcDescription);
+    outputResult(currentResult, calcDescription); //from vendor
 
 } 
+
 
 function add() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult + enteredNumber;
+    currentResult += enteredNumber;
     createAndWriteLog('+', initialResult, enteredNumber);
+    logEntries.push(enteredNumber);
+    console.log(logEntries);
 }
 
 function subtract() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult - enteredNumber;
+    currentResult -= enteredNumber;
     createAndWriteLog('-', initialResult, enteredNumber);
 }
 
 function multiply() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult * enteredNumber;
+    currentResult *= enteredNumber;
     createAndWriteLog('*', initialResult, enteredNumber);
 }
 
 function divide() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult / enteredNumber;
+    currentResult /= enteredNumber;
     createAndWriteLog('/', initialResult, enteredNumber);
 }
 
